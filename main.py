c424b2a20ec9b056385a9fda1268e1f72dd180e5
Simon MWEPU <simonmwepu@gmail.com>
2019-04-07 15:01:38 +0200

    [UPDATE] load pendu.ui

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from PyQt5.uic import loadUiType
 import sys
 import os
 
-from_main, _ = loadUiType(os.path.join(os.path.dirname(__file__), "main.ui"))
+from_main, _ = loadUiType(os.path.join(os.path.dirname(__file__), "pendu.ui"))
 #################################################
 #               Class Main                      #
 #################################################
 class Main(QMainWindow, from_main):
 
     def __init__(self, parent=None):
-        pass
+
+        super(Main, self).__init__(parent)
+        self.setupUi(self)
 
 #main function
 def main():
 
     app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
 
 if __name__=='__main__':
 
