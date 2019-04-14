01e0aed10ce1a935c59ed8ec14d1e2837b660cb9
Simon MWEPU <simonmwepu@gmail.com>
2019-04-14 13:28:01 +0200

    [ADD]connection and cursor

--- model/connection_db.py
+++ model/connection_db.py
@@ -1,2 +1,8 @@
-import _sqlite3
+import sqlite3
 
+#################################################
+#       Connection to DB                        #
+#################################################
+
+connection = sqlite3.connect("../pendu_db.db")
+cursor = connection.cursor()
