106a593a6594941e393ded2ccd55b39721c4c45c
Simon MWEPU <simonmwepu@gmail.com>
2019-04-14 23:27:05 +0200

    [ADD]try to fix table bug

--- model/PenduModel.py
+++ model/PenduModel.py
@@ -6,14 +6,15 @@
 
 cDb = ConnectionDB()"""
 import sqlite3
-connection = sqlite3.connect("pendu.db")
+connection = sqlite3.connect("../pendu.db")
 cursor = connection.cursor()
 class PenduModel():
 
     def select_animal(self):
 
-        request = "SELECT nom FROM animal"
+        request = "SELECT id FROM animal"
         cursor.execute(request)
         result = cursor.fetchall()
 
+        print(result)
         #return result
