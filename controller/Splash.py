20f479a221e92f3266d99fce5e697a7b439f868d
Simon MWEPU <simonmwepu@gmail.com>
2019-04-16 20:34:01 +0200

    [ADD]fix Os error

--- controller/Splash.py
+++ controller/Splash.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QSize
 from PyQt5.uic import loadUiType
 import sys
+import os
 from_choose, _ = loadUiType(os.path.join(os.path.dirname(__file__), "../view/splash.ui"))
 
 class Splash():
