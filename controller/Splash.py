38600b48111b1712d3fab1004d8312f63f65664f
Simon MWEPU <simonmwepu@gmail.com>
2019-04-16 20:41:13 +0200

    [ADD]import all from main

--- controller/Splash.py
+++ controller/Splash.py
@@ -1,8 +1,7 @@
 from PyQt5.QtCore import QSize
-from PyQt5.uic import loadUiType
-import sys
-import os
-from_choose, _ = loadUiType(os.path.join(os.path.dirname(__file__), "../view/splash.ui"))
+from controller.main import *
 
-class Splash():
+from_splash, _ = loadUiType(os.path.join(os.path.dirname(__file__), "../view/splash.ui"))
+
+class Splash(QMainWindow, from_splash):
     pass
