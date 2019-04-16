9a2b6b049c3548458eaded0b6bbb6f5096f6e2c1
Simon MWEPU <simonmwepu@gmail.com>
2019-04-16 21:01:43 +0200

    [ADD]a signal

--- controller/Splash.py
+++ controller/Splash.py
@@ -1,10 +1,12 @@
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import *
 from controller.main import *
+
 
 from_splash, _ = loadUiType(os.path.join(os.path.dirname(__file__), "../view/splash.ui"))
 
 class Splash(QMainWindow, from_splash):
     pass
 
-class Progress():
-    pass
+class Progress(QThread):
+
+    signal = pyqtSignal(int)
